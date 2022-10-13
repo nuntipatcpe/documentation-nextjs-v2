b8c3262672d74ee60da67ef810cdf1e2171708ad
boom <nuntipatcpe@gmail.com>
2022-10-13 12:27:02 +0700

    fead : _document.tsx set Herder

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Documentation V.2</title>
+          <link rel="icon" href="/image/icons8-doc-100.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
