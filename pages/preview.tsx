5fcf1871137ff492f4528e0d7fa4476498fafd8d
boom <nuntipatcpe@gmail.com>
2022-10-15 15:36:52 +0700

    feat : Preview page

---
+++ pages/preview.tsx
@@ -0,0 +1,35 @@
+import Code from "@/components/Code";
+import Markdown from "markdown-to-jsx";
+import React, { useState } from "react";
+
+type Props = {};
+
+export default function Preview({}: Props) {
+  const [text, setText] = useState("");
+  return (
+    <div className="preview">
+      <h1>Write here</h1>
+      <h1>Preview</h1>
+      <textarea
+        className="preview_area"
+        onChange={(e) => setText(e.target.value)}
+      >
+        asdas
+      </textarea>
+
+      <div className="markdown">
+        <Markdown
+          options={{
+            overrides: {
+              code: {
+                component: Code,
+              },
+            },
+          }}
+        >
+          {text}
+        </Markdown>
+      </div>
+    </div>
+  );
+}
